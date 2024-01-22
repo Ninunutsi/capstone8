67e7daa99fba8798528ea6e53a1106d3156bf161
Nino Khurtsidze <khurtsidzenino4@gmail.com>
2024-01-22 22:01:55 +0400

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,22 @@ import Profile from "./pages/Profile";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<IndexRoute />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/library" element={<Library />} />
-          <Route path="/faq" element={<Faqs />} />
-          <Route path="/learn" element={<LayoutLearn />}>
-            <Route index element={<Game1 />} />
-            <Route path="/learn/game2" element={<Game2 />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/capstone8" element={<Layout />}>
+            <Route index element={<IndexRoute />} />
+            <Route path="/capstone8/add" element={<Add />} />
+            <Route path="/capstone8/library" element={<Library />} />
+            <Route path="/capstone8/faq" element={<Faqs />} />
+            <Route path="/capstone8/learn" element={<LayoutLearn />}>
+              <Route index element={<Game1 />} />
+              <Route path="/capstone8/learn/game2" element={<Game2 />} />
+            </Route>
+            <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-        <Route path="*" element={<Wildcard />} />
-      </Routes>
+          <Route path="*" element={<Wildcard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
